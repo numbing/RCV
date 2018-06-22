ea001c028b5bd4e6e5a6eb67ec23c19f5b916a8b
Michal Koudelka <michal.koudelka@labfolder.com>
2018-06-22 13:53:54 +0200

    Add map component

--- src/components/main.js
+++ src/components/main.js
@@ -4,10 +4,20 @@ import Emergency from './emergency'
 import Messages from './messages'
 
 class Main extends Component{
+    markClick(markNumber) {
+      console.log(markNumber)
+    }
     render(){
         return(
             <div>
-                <Map/>
+              <Map
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCOyvijzxBB3EocKRr1aoncrfrPWrOvck&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+                onMarkerClick={this.markClick}
+
+              />
                 <Emergency/>
                 <Messages/>
 
--- src/components/map.js
+++ src/components/map.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class Map extends Component {
-    render() {
-        return (
-            <div>
-                <div>maps</div>
-            </div>
-        )
-    }
-}
+const MapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 52.52437, lng: 13.41053 }}>
+    <Marker position={{ lng: 13.496125, lat: 52.557568 }} onClick={e => props.onMarkerClick(1)} />
+    <Marker position={{ lng: 13.19083257, lat: 52.53416453 }} onClick={e => props.onMarkerClick(2)} />
+    <Marker position={{ lng: 13.06566, lat: 52.39886 }} onClick={e => props.onMarkerClick(3)} />
+  </GoogleMap>
+));
 
-export default Map
+
+export default MapComponent;
