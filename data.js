415bfc57703fdfb228e97e41130216f2f1e3c094
mohammad.seyedsaidi <mirnumbing@gmail.com>
2018-06-22 16:59:28 +0200

    data

---
+++ data.js
@@ -0,0 +1,73 @@
+export default  {
+  "camps": {
+    "1": {
+      "name": "Berlin-Neu-Hohenschönhausen",
+      "maxPeople": 180,
+      "atmPeople": 220,
+      "missingThings": [
+        "Winter clothes",
+        "Baby food",
+        "Coffee"
+      ],
+      "missingPeople": [
+        "Plumbers"
+      ],
+      "lastUpdate": "21.6.2018",
+      "messages": [{
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "author": "Ziggy Stardust"
+      },
+        {
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "author": "Barber Ann"
+        }
+      ]
+    },
+    "2": {
+      "name": "Berlin-spandau",
+      "maxPeople": 120,
+      "atmPeople": 50,
+      "missingThings": [
+        "jacket",
+        "food",
+        "drink"
+      ],
+      "missingPeople": [
+        "doctor"
+      ],
+      "lastUpdate": "13.2.2018",
+      "messages": [{
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "author": "Ziggy Stardust"
+      },
+        {
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "author": "Barber Ann"
+        }
+      ]
+    },
+    "3": {
+      "name": "Berlin-potsdam",
+      "maxPeople": 75,
+      "atmPeople": 99,
+      "missingThings": [
+        "Summer clothes",
+        "Baby food",
+        "Milk"
+      ],
+      "missingPeople": [
+        "Teacher"
+      ],
+      "lastUpdate": "21.6.2018",
+      "messages": [{
+        "text": "d do eiusmod tempor incididunt ut lLorem ipsum dolor sit amet, consectetur adipiscing elit, seabore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "author": "Ziggy Stardust"
+      },
+        {
+          "text": "mpor incididunt ut labore et doloLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tere magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "author": "Barber Ann"
+        }
+      ]
+    }
+  }
+}
--- src/components/emergency.js
+++ src/components/emergency.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import Data from './data';
 
 class Emergency extends Component {
+
     render() {
+        console.log(Data)
         return (
             <div>
+
                 <div>emergency</div>
+
             </div>
         )
     }
